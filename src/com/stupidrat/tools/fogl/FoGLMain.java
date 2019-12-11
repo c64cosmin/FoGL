afed15e12d901880224ebbf3a4e85844e4112d90
c64cosmin <c64cosmin@gmail.com>
2019-12-12 01:42:21 +0200

    Have two separated Mains

---
+++ src/com/stupidrat/tools/fogl/FoGLMain.java
@@ -0,0 +1,6 @@
+package com.stupidrat.tools.fogl;
+public class FoGLMain {
+    public static void main(String[] args){
+        new FoGL();
+    }
+}
--- src/com/stupidrat/tools/fogl/Main.java
+++
@@ -1,7 +0,0 @@
-package com.stupidrat.tools.fogl;
-public class Main {
-    public static void main(String[] args){
-        if(args.length == 0)new FoGL();
-        else if(args[0].equals("--sprite"))new SoGL();
-    }
-}
